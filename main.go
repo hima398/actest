c9d508b30b2a6ae55a323cd452dd3c80b958ea37
hinata398 <r.hinata0398@gmail.com>
2020-11-12 22:50:28 +0900

    wip file output

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 )
 
 func main() {
 	var n int
 	fmt.Scan(&n)
 	max := 100
+	file, err := os.OpenFile("./in.txt", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return
+	}
+	defer file.Close()
 	for i := 0; i < n; i++ {
 		v := rand.Int() % max
 		fmt.Printf("%d ", v)
