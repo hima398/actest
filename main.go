a11da7d7bffa8961f331269d80113e7cfd9c0d04
hinata398 <r.hinata0398@gmail.com>
2020-11-14 09:51:16 +0900

    add interval

--- main.go
+++ main.go
@@ -2,26 +2,32 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
 )
 
 func main() {
-	var n int
-	fmt.Scan(&n)
+	n := 200000
 	max := 100
-	file, err := os.OpenFile("./in.txt", os.O_WRONLY|os.O_CREATE, 0600)
+	interval := 1000
+	fileName := "in.txt"
+	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return
 	}
 	defer file.Close()
 	w := bufio.NewWriter(file)
 	w.WriteString(strconv.Itoa(n) + "\n")
+	s := ""
 	for i := 0; i < n; i++ {
 		v := rand.Int() % max
-		w.WriteString(strconv.Itoa(v) + " ")
+		s += strconv.Itoa(v) + " "
+		if i%interval == 0 {
+			w.WriteString(s)
+			w.Flush()
+		}
 	}
+	w.WriteString(s)
 	w.Flush()
 }
